09f45e85110f8badcc2d2b9468134c5a89550bf1
alraune91 <149472015+alraune91@users.noreply.github.com>
2023-12-01 23:14:51 +0600

    Add files via upload

---
+++ AS_project_4 — копия/app/src/main/java/com/example/as_project_4/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.as_project_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private LinearLayout mLL;
+    private TextView mTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLL = findViewById(R.id.linearLayout);
+        mTV = findViewById(R.id.textView);
+    }
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.buttonRed) {
+            mTV.setText(R.string.red);
+            mLL.setBackgroundColor(ContextCompat.getColor(this, R.color.red));
+        } else if (id == R.id.buttonYellow) {
+            mTV.setText(R.string.yellow);
+            mLL.setBackgroundColor(ContextCompat.getColor(this, R.color.yellow));
+        } else if (id == R.id.buttonGreen) {
+            mTV.setText(R.string.green);
+            mLL.setBackgroundColor(ContextCompat.getColor(this, R.color.green));
+        }
+    }
+}
